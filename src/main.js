c7a3f460049c9e5ab7eb93330247b6ea17a69b22
QuinnCallander <qcalland@gmail.com>
2025-10-28 15:15:53 -0400

    Added the requirements listed in demo09 part 5

--- src/main.js
+++ src/main.js
@@ -96,10 +96,9 @@ async function displayCardsDynamically() {
             // Populate the card with hike data
             newcard.querySelector('.card-title').textContent = hike.name;
             newcard.querySelector('.card-text').textContent = hike.details || `Located in ${hike.city}.`;
-            newcard.querySelector('.card-length').textContent = hike.length;
-
-            // 👇 ADD THIS LINE TO SET THE IMAGE SOURCE
+            newcard.querySelector('.card-length').textContent = hike.length
             newcard.querySelector('.card-image').src = `./images/${hike.code}.jpg`;
+            newcard.querySelector(".read-more").href = `eachHike.html?docID=${doc.id}`;
 
             // Attach the new card to the container
             document.getElementById("hikes-go-here").appendChild(newcard);
